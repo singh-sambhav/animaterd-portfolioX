8860d1d276b09256e83ff2dc7dd7dc5517fa4c50
sambhav <sambhav962@gmail.com>
2023-11-02 01:39:58 +0530

    Third commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,21 @@ import './Hero.scss'
  const Hero = () => {
   return (
     <div className='hero'>
-      
+      <div className="wrapper">
+
+        <div className="textContainer">
+          <h2>Sambhav Singh</h2>
+          <h1>Web Developer</h1>
+          <div className="buttons">
+            <button>See the Latest Works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="/scroll.png" alt="" />
+        </div>
+      </div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="" style={{width:'608px'}}/>
+      </div>
     </div>
   )
 }
