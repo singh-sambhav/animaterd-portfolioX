0f91bfbb6363adad05a8d1342eef4b8024c1ec37
sambhav <sambhav962@gmail.com>
2023-11-03 01:47:25 +0530

    Parallax

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss"
 import { Navbar } from "./components/Navbar";
 import Hero from "./components/hero/Hero";
 import { Parallax } from "./components/parallax/Parallax";
+import { Services } from "./components/services/Services";
 
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
       <Hero/>
     </section>
     <section id='Services'><Parallax type="services"/></section>
-    <section>services</section>
+    <section><Services/></section>
     {/* <section>Parallax</section> */}
     <section id="Portfolio"><Parallax type="portfolio"/></section>
     <section>Portfolio1</section>
--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -5,12 +5,16 @@ export const Parallax = ({type}) => {
     const ref = useRef()
 
     const {scrollYProgress} = useScroll({
-        target:ref
+        target:ref,
+        offset:["start start","end start"]
     })
 
-    const yBg=useTransform(scrollYProgress,[0,1],['0%','100%'])
+    const yText=useTransform(scrollYProgress,[0,1],['0%','300%'])
+    const yBg=useTransform(scrollYProgress,[0,1],['0%','90%'])
   return (
-    <div className='parallax' 
+    <div 
+        className='parallax'
+        ref = {ref} 
         style={{
             background:
             type==='services'
@@ -18,10 +22,14 @@ export const Parallax = ({type}) => {
             :'linear-gradient(180deg,#111132,#505064)'
         }}
     >
-        <motion.h1 style={{y:yBg}}>{type==='services'?"What We Do?": "What We Did?"}</motion.h1>
+        <motion.h1 style={{y:yText}}>{type==='services'?"What We Do?": "What We Did?"}</motion.h1>
         <motion.div className="mountains"></motion.div>
-        <motion.div className="planets"></motion.div>
-        <motion.div className="stars"></motion.div>
+        <motion.div className="planets" 
+        style=
+        {{y:yBg,
+            backgroundImage: `url(${type==="services" ? "/planets.png":"/sun.png"})`
+        }}></motion.div>
+        <motion.div style={{x:yBg}} className="stars"></motion.div>
 
 
     </div>
