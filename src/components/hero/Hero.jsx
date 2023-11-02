cc8a42acb706687b853dd195801df0d0cd2dd84d
sambhav <sambhav962@gmail.com>
2023-11-02 16:42:56 +0530

    Sliding Text Animation

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,20 +1,65 @@
 import React from 'react'
-import './Hero.scss'
+import {motion} from 'framer-motion';
+import './Hero.scss';
+
+const textVariants = {
+  initial:{
+    x:-500,
+    opacity:0,
+  },
+  animate:{
+    x:0,
+    opacity:1,
+    transition:{
+      duration:1,
+      staggerChildren:0.1,
+    },
+  },
+  scrollButton:{
+    opacity:0,
+    y:10,
+    transition:{
+      duration:2,
+      repeat:Infinity,
+    },
+  },
+};
+
+const sliderVariants = {
+  initial:{
+    x:0,
+  },
+  animate:{
+    x:'-220%',
+    transition:{
+      repeat:Infinity,
+      repeatType:"mirror",
+      duration:20,
+    },
+  },
+};
  const Hero = () => {
   return (
     <div className='hero'>
       <div className="wrapper">
-
-        <div className="textContainer">
-          <h2>Sambhav Singh</h2>
-          <h1>Web Developer</h1>
-          <div className="buttons">
-            <button>See the Latest Works</button>
-            <button>Contact Me</button>
-          </div>
-          <img src="/scroll.png" alt="" />
-        </div>
+        <motion.div 
+        className="textContainer" 
+        variants={textVariants} 
+        initial="initial" 
+        animate="animate"
+        >
+          <motion.h2 variants={textVariants}>SAMBHAV SINGH</motion.h2>
+          <motion.h1 variants={textVariants}>Web Developer</motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>See the Latest Works</motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="" />
+        </motion.div>
       </div>
+      <motion.div className="slidingTextContainer" variants={sliderVariants} initial="initial" animate="animate">
+       Software Developer
+      </motion.div>
       <div className="imageContainer">
         <img src="/hero.png" alt="" style={{width:'608px'}}/>
       </div>
