32b1bff4688f62f6701266df2d50e4afb0060d1d
sambhav <sambhav962@gmail.com>
2023-11-02 18:06:38 +0530

    parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss"
 import { Navbar } from "./components/Navbar";
 import Hero from "./components/hero/Hero";
+import { Parallax } from "./components/parallax/Parallax";
 
 
 const App = () => {
@@ -9,10 +10,11 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    <section id='Services'>Parallax</section>
+    <section id='Services'><Parallax type="services"/></section>
     <section>services</section>
-    <section>Parallax</section>
-    <section id="Portfolio">Portfolio1</section>
+    {/* <section>Parallax</section> */}
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
+    <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
     <section id='Contact'>Contact</section>
---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react'
+import "./parallax.scss"
+import {motion,useScroll, useTransform} from "framer-motion"
+export const Parallax = ({type}) => {
+    const ref = useRef()
+
+    const {scrollYProgress} = useScroll({
+        target:ref
+    })
+
+    const yBg=useTransform(scrollYProgress,[0,1],['0%','100%'])
+  return (
+    <div className='parallax' 
+        style={{
+            background:
+            type==='services'
+            ?'linear-gradient(180deg,#111132,#0c0c1d)'
+            :'linear-gradient(180deg,#111132,#505064)'
+        }}
+    >
+        <motion.h1 style={{y:yBg}}>{type==='services'?"What We Do?": "What We Did?"}</motion.h1>
+        <motion.div className="mountains"></motion.div>
+        <motion.div className="planets"></motion.div>
+        <motion.div className="stars"></motion.div>
+
+
+    </div>
+  )
+}
