6b5227cc48c0a16fcff926f740ec7b696ed7e46c
sambhav <sambhav962@gmail.com>
2023-11-07 13:14:22 +0530

    Featured Works Section In Process

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
-import "/portfolio.scss"
+import {React, useRef} from 'react'
+import "./portfolio.scss"
+import { motion , useScroll , useSpring, useTransform} from 'framer-motion';
 
-export const Portfolio = () => {
+const items = [
+  {
+    id:1,
+    title:"Netflix Clone",
+    img:"https://images.pexels.com/photos/987586/pexels-photo-987586.jpeg?auto=compress&cs=tinysrgb&w=600",
+    desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, voluptatem.",
+  },
+  {
+    id:2,
+    title:"Fashify",
+    img:"https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=600",
+    desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, voluptatem.",
+  },
+  {
+    id:3,
+    title:"News Ride",
+    img:"https://images.pexels.com/photos/3944377/pexels-photo-3944377.jpeg?auto=compress&cs=tinysrgb&w=600",
+    desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, voluptatem.",
+  },
+  {
+    id:4,
+    title:"I-Notebook",
+    img:"https://images.pexels.com/photos/3944377/pexels-photo-3944377.jpeg?auto=compress&cs=tinysrgb&w=600",
+    desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, voluptatem.",
+  },
+];
+
+const Single = ({item})=>{
+  const ref = useRef();
+
+  const {scrollYProgress} = useScroll({
+    target:ref,
+  });
+
+  const y = useTransform(scrollYProgress,[0,1],[-300,300]);
+
   return (
-    <div className='portfolio'>Portfolio</div>
+    <section ref={ref}>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imgContainer">
+        <img src={item.img} alt=""/>
+          </div>
+          <motion.div className="textContainer" style={{y}}>
+            <h2 >{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export const Portfolio = () => {
+
+  const ref = useRef()
+  const {scrollYProgress} = useScroll({
+    target:ref,
+    offset:["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress,{
+    stiffness:100,
+    damping:30,
+  });
+  return (
+    <div className='portfolio' ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{scaleX}}className="progressBar">
+
+        </motion.div>
+      </div>
+      {items.map(item=>(
+        <Single item ={item} key={item.id}/>
+      ))}
+    </div>
   )
 }
