6b216bd57b7e3e58e3b4b11037e140f4e98342be
sambhav <sambhav962@gmail.com>
2023-11-08 02:04:19 +0530

    Contacts Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.scss"
 import { Navbar } from "./components/Navbar";
+import { Contact } from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import { Parallax } from "./components/parallax/Parallax";
 import { Portfolio } from "./components/portfolio/Portfolio";
@@ -17,7 +18,8 @@ const App = () => {
     {/* <section>Parallax</section> */}
     <section id="Portfolio"><Parallax type="portfolio"/></section>
     <Portfolio/>
-    <section id='Contact'>Contact</section>
+    <section id='Contact'><Contact/></section>
+    
   </div>;
 };
 
